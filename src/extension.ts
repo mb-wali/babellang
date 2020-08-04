03b2d080642c4d0efdc0270ec98adccf2e21c7af
Beraat ALDEMİR (ULAKBİM) <berataldemir@gmail.com>
2020-08-04 15:15:36 +0300

    removed some comments and unnecessary code parts

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,6 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "babellang" is now active!');
 
 	const disposable = vscode.commands.registerCommand('babellang.trans1', function () {
 		// Get the active text editor
@@ -55,5 +47,4 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable2);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() { }
